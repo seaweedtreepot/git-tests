833ad4d6309630ce7416dc3ce2a93c318ce45bd1
dong <variable9820@daum.net>
2024-10-13 12:12:37 +0900

    create cal.py

---
+++ cal.py
@@ -0,0 +1,11 @@
+class FourCal:
+    def __init__(self,first,second)
+        self.first = first
+        self.second  = second
+
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+    def add(self):
+        result = self.first + self.second
+        return result
